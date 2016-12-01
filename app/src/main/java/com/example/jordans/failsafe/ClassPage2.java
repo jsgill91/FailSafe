0742b3770cdf050da85157dd6176e55400e58a12
Jordan Gill <jsgill@crismon.ua.edu>
2016-12-01 13:17:36 -0600

    Added class page xmls

---
+++ app/src/main/java/com/example/jordans/failsafe/ClassPage2.java
@@ -0,0 +1,14 @@
+package com.example.jordans.failsafe;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class ClassPage2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_class_page2);
+    }
+
+}
